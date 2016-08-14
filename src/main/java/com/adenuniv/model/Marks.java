0f7727db2d3a7e44531e02f35d59bc8142f1c193
Rashad Saif <rashad_saif@yahoo.com>
2016-08-14 17:17:53 +0300

    fix mark error

--- src/main/java/com/adenuniv/model/Marks.java
+++ src/main/java/com/adenuniv/model/Marks.java
@@ -1,13 +1,23 @@
 package com.adenuniv.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import lombok.Data;
 @Data
 @Entity
 public class Marks {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@JsonProperty(access = Access.READ_ONLY)
+	private Long id;
 	@ManyToOne
 	@JoinColumn(name = "SUBJECT_ID")
 	private Subject subject;
