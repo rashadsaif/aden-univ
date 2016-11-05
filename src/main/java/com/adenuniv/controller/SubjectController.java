a42687083c82fc3eaee3bae91006e1011e2b3f96
rsaif <rashad_saif@yahoo.com>
2016-11-05 18:17:51 +0300

    crossorgin

--- src/main/java/com/adenuniv/controller/SubjectController.java
+++ src/main/java/com/adenuniv/controller/SubjectController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,8 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired) )
 @Api("Operations about Subject")
+@CrossOrigin(origins = "*")
+
 public class SubjectController {
 	
 	private final SubjectRepository repository;
